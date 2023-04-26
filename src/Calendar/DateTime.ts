cb78af3d52b44808ba47a60eb4ac2d324fcd54d3
sreyrea <hansreyrea2222@gmail.com>
2023-04-26 19:54:00 +0700

    create folder

---
+++ src/Calendar/DateTime.ts
@@ -0,0 +1,8 @@
+export class DateTime{
+    private Date:string
+    private Time:string
+    constructor(Date:string, Time:string){
+        this.Date = Date
+        this.Time = Time
+    }
+}
---
+++ src/Person/Person.ts
@@ -0,0 +1,10 @@
+export class Person{
+    private firstName: string
+    private lastName: string
+    private age: number
+    constructor (firstName: string,lastName:string, age: number){
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age 
+    }
+}
