cf2869c89e5d8d659c3c5b55714bb3a9ebd90fdc
sreyrea <hansreyrea2222@gmail.com>
2023-04-28 22:00:44 +0700

    Add function in pilot

--- src/Person/Pilot.ts
+++ src/Person/Pilot.ts
@@ -1,8 +1,19 @@
+import { DateTime } from "../Calendar/DateTime";
+import { Flight } from "../Flight/Flight";
 import { Employee } from "./Employee";
 import { Gender } from "./Gender";
 
 export class Pilot extends Employee{
+    private flights:Flight[]=[];
+    private dates:DateTime[]=[];
     constructor(salary: number,firstName: string,lastName: string,age: number,gender:Gender){
         super(salary,firstName,lastName,age,gender);
+    }
+    addFlight(flight:Flight){
+        this.flights.push(flight)
+    }
+    addDateFlight(date:DateTime){
+        this.dates.push(date)
+
     }
 }
