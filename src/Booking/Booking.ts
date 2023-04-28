0f016a18035bb94e5af37ef6d474747f77fc9af2
NetSe24 <netse0962@gmail.com>
2023-04-29 00:22:08 +0700

    Update

--- src/Booking/Booking.ts
+++ src/Booking/Booking.ts
@@ -3,7 +3,8 @@ import { BookingTrip } from "./BookingTrip"
 export class Booking {
     private passengers: Passenger[] = [];
     private bookingTrip: BookingTrip[] = [];
-    constructor(private bookingReferenceNumber: string) {
+    constructor(private bookingReferenceNumber: string, private bookingId:string) {
         this.bookingReferenceNumber = bookingReferenceNumber;
+        this.bookingId = bookingId;
     }
 }
