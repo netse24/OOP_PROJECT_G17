2108c4c769f2b5f0a46e5195704542232ad1fd1e
sreyrea <hansreyrea2222@gmail.com>
2023-04-29 15:58:26 +0700

    update

---
+++ src/Main.ts
@@ -0,0 +1,22 @@
+import { BookingFlight } from "./Booking/BookingFlight";
+import { Meal } from "./Booking/Meal";
+import { Flight } from "./Flight/Flight";
+import { Chef } from "./Person/Chef";
+import { Gender } from "./Person/Gender";
+
+
+// create booking flight
+let bookingFlight = new BookingFlight();
+bookingFlight.setAMealFromBookingFlight(Meal.VEGENTARIAN);
+// create chef
+let chef = new Chef(200,'Net','Se',19,Gender.MALE)
+// create flight 
+let flight = new Flight('AD1');
+flight.setMeal();
+chef.getMeals()
+console.log(bookingFlight)
+// create passenger 
+// let passenger1 =
+
+
+//  As an airline manager, I want to find out how much salary I pay all my employees.
